23c5f88ea765a8ff00135051f7d71fa323a0c0fd
Antipov Sergey <seregad544@gmail.com>
2022-07-07 21:37:58 +0300

    step 4.4

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/seregad544/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/seregad544/frontend-project-lvl2/actions)
+[![Actions Status](https://github.com/seregad544/frontend-project-lvl2/actions/workflows/run-test-and-lint.yml/badge.svg)](https://github.com/seregad544/frontend-project-lvl2/actions/workflows/run-test-and-lint.yml)
 [![Maintainability](https://api.codeclimate.com/v1/badges/7652fa969a2b19996651/maintainability)](https://codeclimate.com/github/seregad544/frontend-project-lvl2/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/7652fa969a2b19996651/test_coverage)](https://codeclimate.com/github/seregad544/frontend-project-lvl2/test_coverage)
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import getComparisonFile from '../index.js';
 
-const a = `{
+const test1 = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -9,6 +9,39 @@ const a = `{
   + verbose: true
 }`;
 
+const test2 = `{
+  - follow: false
+  + follow: true
+  - host: hexlet.io
+  + host: vk.com
+    proxy: 123.234.53.22
+    timeout: 50
+  + timezone: 3
+}`;
+
+const test3 = `{
+    follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  + proxy: 134.434.53.22
+    timeout: 50
+  + verbose: true
+}`;
+
+const test4 = `{
+  + follow: true
+  - host: hexlet.io
+  + host: vk.com
+  + proxy: 123.234.53.22
+  - timeout: 20
+  + timeout: 50
+  + timezone: 3
+  - verbose: true
+}`;
+
 test('getComparisonFile', () => {
-  expect(getComparisonFile('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(a);
+  expect(getComparisonFile('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(test1);
+  expect(getComparisonFile('__fixtures__/file1.json', '__fixtures__/file3.json')).toEqual(test2);
+  expect(getComparisonFile('__fixtures__/file1.json', '__fixtures__/file4.json')).toEqual(test3);
+  expect(getComparisonFile('__fixtures__/file2.json', '__fixtures__/file3.json')).toEqual(test4);
 });
