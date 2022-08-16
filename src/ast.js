fa77f36cc4b6458d6c02757d01b24b4cb1021ed7
Antipov Sergey <seregad544@gmail.com>
2022-08-16 11:50:29 +0300

    refactoring 1.6.1

--- src/ast.js
+++ src/ast.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 
-const buildNode = (data1, data2, key) => {
+const buildNode = (data1, data2, key, parentFunction) => {
   if (!_.has(data1, key)) return { key, status: 'added', value: data2[key] };
   if (!_.has(data2, key)) return { key, status: 'removed', value: data1[key] };
   if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-    return { key, status: 'nested', children: buildTree(data1[key], data2[key]) };
+    return { key, status: 'nested', children: parentFunction(data1[key], data2[key]) };
   }
   if (_.isArray(data1[key]) && _.isArray(data2[key]) && _.isEqual(data1[key], data2[key])) {
     return { key, status: 'unupdated', value: data1[key] };
@@ -19,7 +19,7 @@ const buildNode = (data1, data2, key) => {
 
 const buildTree = (data1, data2) => {
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
-  return keys.map((key) => buildNode(data1, data2, key));
+  return keys.map((key) => buildNode(data1, data2, key, buildTree));
 };
 
 export default buildTree;
